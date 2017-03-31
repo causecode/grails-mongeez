b21f18f4fb981fa9e262b756e292a1cc9661dca5
Shivam Pandey <shivam.pandey@causecode.com>
2017-03-31 15:34:48 +0530

    Removed deprecated class and added org.apache.commons.lang3.time to work with date and time format

--- grails-app/services/grails/mongeez/MongeezService.groovy
+++ grails-app/services/grails/mongeez/MongeezService.groovy
@@ -19,7 +19,7 @@ package grails.mongeez
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
 import com.mongodb.QueryBuilder
-import org.apache.commons.lang.time.DateFormatUtils
+import org.apache.commons.lang3.time.DateFormatUtils
 import org.mongeez.dao.MongeezDao
 import org.mongeez.reader.ChangeSetReaderFactory
 
@@ -64,7 +64,7 @@ class MongeezService {
         // TODO: get a single way to determine the true order of existing executions... finer grained date
         def sort = new BasicDBObject([date: 1, resourcePath: 1])
         def changeSetExecutions = dbColl.find(query).sort((DBObject)sort).toArray()
-        def dateFormatter = DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT
+        def dateFormatter = DateFormatUtils.ISO_8601_EXTENDED_DATETIME_TIME_ZONE_FORMAT
         def datePattern = dateFormatter.getPattern()
         def dateParser = new SimpleDateFormat(datePattern)
         changeSetExecutions.each { execution ->
